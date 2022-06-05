2ceb732ba8e979e6bc8a793a92ab01aa9dfe9092
luhuadong <luhuadong@163.com>
2022-06-05 19:02:59 +0800

    fix baidu 统计

--- source/_static/js/baidutongji.js
+++ source/_static/js/baidutongji.js
@@ -1,9 +1,7 @@
-<script>
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?68129494b0aa8556baf9626ec5ce6189";
+  hm.src = "https://hm.baidu.com/hm.js?2068c52a45001c10c2865823b40d9355";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-</script>
